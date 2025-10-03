cd86c65d4084b30f5962411bc331cbe14af9d9b3
Ashley Giang <tgiang1@ewu.edu>
2025-10-03 13:37:32 -0700

    Add FileLogger

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿
 namespace Logger.Tests;
 
 [TestClass]
---
+++ Logger/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        private readonly string _filePath;
+
+        public FileLogger(string filePath) 
+        {
+            _filePath = filePath;
+        }
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string logLine = $"{DateTime.Now} {ClassName} {logLevel}: {message}";
+            File.AppendAllText(_filePath, logLine + Environment.NewLine);
+        }
+
+    }
+}
