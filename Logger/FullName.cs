c02b5819246f3a5b43ebee3189359065021b95c8
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-10-18 06:59:26 -0700

    create full name record

---
+++ Logger/FullName.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+namespace Logger;
+
+/// <summary>
+/// An immutable value that travels with person-like entities.
+///Chosen as a record struct because:
+/// -it's small, commonly copied, and benefits from value semantics
+/// -default immutability is desired
+///</summary>
+
+public readonly record struct FullName (
+    string First,
+    string Last,
+    string? Middle = null)
+{ /// <summary>
+  /// Displays as "Last, First M." if Middle is present. Else, "Last, First".
+  /// </summary>
+  
+    public override string ToString() =>
+        Middle is { Length: > 0}
+            ? $"{Last}, {First} {Middle[0]}."
+            : $"{Last}, {First}";
+}
