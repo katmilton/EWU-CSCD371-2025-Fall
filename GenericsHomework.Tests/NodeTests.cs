798fad8776a5395d22116b3c9efc6cefc09799c7
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-10-25 12:53:15 -0700

    update NodeTests.cs

--- GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework.Tests/NodeTests.cs
@@ -5,7 +5,30 @@
         [Fact]
         public void NewNode_HasSelfLoopInNext()
         {
+            var n = new Node<int>(42);
+            Assert.NotNull(n.Next);
+            Assert.Same(n, n.Next);
+        }
 
+        [Fact]
+        public void ToString_DelegatesToValueToString_ForValueTypes()
+        {
+            var n = new Node<int>(123);
+            Assert.Equal("123", n.ToString());
+        }
+
+        [Fact]
+        public void ToString_HandlesNullableReferenceValue()
+        {
+            var n = new Node<string?>(null);
+            Assert.Equal(string.Empty, n.ToString());
+        }
+
+        [Fact]
+        public void Next_HasPrivateSetter()
+        {
+            var n = new Node<int>(1);
+            Assert.Same(n, n.Next);
         }
     }
 }
