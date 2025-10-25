6426e7343936900c169ab732b3135ed89462c97e
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-10-25 12:55:34 -0700

    update Node.cs, elaborate on Next non-nullability

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -20,7 +20,7 @@
 
         ///<summary>
         /// The next node in the circular list. If this is the only node, Next points to itself.
-        /// Non-nullable.
+        /// Non-nullable per assignment guidelines; initialized in constructor.
         /// </summary>
         public Node<T> Next { get; set; }
 
