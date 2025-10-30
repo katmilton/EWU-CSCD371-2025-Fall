b628b9490c647094809c64325184a0d5b55f9baa
steeley21 <ksteele5@ewu.edu>
2025-10-30 12:12:34 -0700

    Updated VennDiagramTests for readability

--- GenericsHomework.Tests/VennDiagramTests.cs
+++ GenericsHomework.Tests/VennDiagramTests.cs
@@ -14,8 +14,10 @@ public class VennDiagramTests
         var vd = new VennDiagram<string>();
         var a = vd.AddCircle("A");
         var b = vd.AddCircle("B");
-        a.Add("x"); a.Add("y");
-        b.Add("y"); b.Add("z");
+        a.Add("x"); 
+        a.Add("y");
+        b.Add("y"); 
+        b.Add("z");
 
         var intersection = vd.Intersection("A", "B").ToList();
         Assert.AreEqual<int>(1, intersection.Count);
@@ -28,8 +30,10 @@ public class VennDiagramTests
         var vd = new VennDiagram<string>();
         var a = vd.AddCircle("A");
         var b = vd.AddCircle("B");
-        a.Add("x"); a.Add("y");
-        b.Add("y"); b.Add("z");
+        a.Add("x"); 
+        a.Add("y");
+        b.Add("y"); 
+        b.Add("z");
 
         var union = vd.Union("A", "B").OrderBy(s => s).ToList();
         CollectionAssert.AreEqual(ExpectedUnion, union);
@@ -41,8 +45,10 @@ public class VennDiagramTests
         var vd = new VennDiagram<string>();
         var a = vd.AddCircle("A");
         var b = vd.AddCircle("B");
-        a.Add("x"); a.Add("y");
-        b.Add("y"); b.Add("z");
+        a.Add("x"); 
+        a.Add("y");
+        b.Add("y"); 
+        b.Add("z");
 
         var diff = vd.Difference("A", "B").ToList();
         Assert.AreEqual<int>(1, diff.Count);
