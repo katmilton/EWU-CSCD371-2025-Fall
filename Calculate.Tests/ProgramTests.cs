3bf16bb903f22ebdf2d89ab34757371cba2c27df
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-11-01 14:31:02 -0700

    create project files

---
+++ Calculate.Tests/ProgramTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculate.Tests;
+
+[TestClass]
+public sealed class ProgramTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ Calculate/Calculate/Calculator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public sealed class Calculator
+{
+	public Class1()
+	{
+	}
+}
