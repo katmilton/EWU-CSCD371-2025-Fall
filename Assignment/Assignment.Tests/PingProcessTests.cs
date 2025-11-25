e3f818688b5ea8de76b3981799ed328a7fdb0de1
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-11-24 17:05:41 -0800

    update PingProcessTests

--- Assignment/Assignment.Tests/PingProcessTests.cs
+++ Assignment/Assignment.Tests/PingProcessTests.cs
@@ -57,8 +57,10 @@ public class PingProcessTests
     [TestMethod]
     public void RunTaskAsync_Success()
     {
-        // Do NOT use async/await in this test.
-        // Test Sut.RunTaskAsync("localhost");
+        Task<PingResult> task = Sut.RunTaskAsync("localhost");
+        PingResult result = task.Result;
+
+        AssertValidPingOutput(result);
     }
 
     [TestMethod]
