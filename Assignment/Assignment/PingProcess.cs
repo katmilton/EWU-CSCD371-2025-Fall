6775f659729c1c80b948bb52cd7d4a6271093c3b
Kat Milton <40556351+katmilton@users.noreply.github.com>
2025-11-24 16:47:20 -0800

    implement RunTaskAsync

--- Assignment/Assignment/PingProcess.cs
+++ Assignment/Assignment/PingProcess.cs
@@ -27,7 +27,16 @@ public class PingProcess
 
     public Task<PingResult> RunTaskAsync(string hostNameOrAddress)
     {
-        throw new NotImplementedException();
+        return Task.Run(() =>
+        { 
+            StartInfo.Arguments = hostNameOrAddress;
+            StringBuilder? stringBuilder = null;
+            void updateStdOutput(string? line) =>
+                (stringBuilder ??= new StringBuilder()).AppendLine(line);
+
+            Process process = RunProcessInternal(StartInfo, updateStdOutput, default, default);
+            return new PingResult(process.ExitCode, stringBuilder?.ToString());
+        });
     }
 
     async public Task<PingResult> RunAsync(
